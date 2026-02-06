7f13151a284232f9942e7cf39c33a834e6621c88
nurlcca <lh52174@truman.edu>
2026-02-06 20:23:04 +0600

    lesson2hw

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -18,9 +18,31 @@ class MageHero(Hero):
     def action(self):
         return f"This is new method for child class {self.nick}"
 
-asuna = Hero("Asuna", 999, 9999)
-mage_kirito = MageHero("Ardager", 100, 1000, 500)
+#asuna = Hero("Asuna", 999, 9999)
+#mage_kirito = MageHero("Ardager", 100, 1000, 500)
 
+#print(asuna.action())
+#print(mage_kirito.action())
 
-print(asuna.action())
-print(mage_kirito.action())
+class Animal:
+    def action(self):
+        print(f"Animal action")
+
+class Fly(Animal):
+    def action(self):
+        super().action()
+        print(f"Fly action")
+
+class Swim(Animal):
+    def action(self):
+        # super().action()
+        print(f"Swin action")
+
+class Duck(Fly,Swim):
+    def action(self):
+        super().action()
+        print(f"Dunk action")
+
+donald_duck = Duck()
+donald_duck.action()
+print(Duck.__mro__)
