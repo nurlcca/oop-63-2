66bcfd0869698844b1f6439956ad8563a054e14e
nurlcca <lh52174@truman.edu>
2026-02-10 20:43:21 +0600

    lesson3hw

--- hw/lesson3hw.py
+++ hw/lesson3hw.py
@@ -1,6 +1,5 @@
 import random
 
-
 class UserAccount:
     def __init__(self, username, login, password, balance):
         self.username = username          # public
@@ -31,14 +30,46 @@ class UserAccount:
         return self._balance
 
 
-account = UserAccount("nuraiym", "nuraiym", 1234, 5000)
+#account = UserAccount("nuraiym", "nuraiym", 1234, 5000)
 
-account.check_access("nuraiym", 1234)
-account.change_password("nuraiym")
-print(account.get_balance())
+#account.check_access("nuraiym", 1234)
+#account.change_password("nuraiym")
+#print(account.get_balance())
 
-try:
-    print(account.__password)
-except AttributeError:
-    print("No access to private attribute")
+#try:
+    #print(account.__password)
+#except AttributeError:
+    #print("No access to private attribute")
+
+from abc import ABC, abstractmethod
+class NotificationService(ABC):
+    @abstractmethod
+    def send_to_phone(self, phone, text):
+        pass
+    @abstractmethod
+    def send_to_email(self, email, text):
+        pass
+
+class SMSService(NotificationService):
+    def send_to_phone(self, phone, text):
+        print(f"Sending SMS to {phone}")
+    
+    def send_to_email(self, email, text):
+        print("SMS can't be sent to email")
+
+class EmailService(NotificationService):
+    def send_to_phone(self, phone, text):
+        print("Email can't be sent to phone")
+    
+    def send_to_email(self, email, text):
+        print(f"Sending email to {email}")
+
+sms = SMSService()
+email = EmailService()
+
+sms.send_to_phone("+996700000000", "Your code: 1234")
+sms.send_to_email("test@gmail.com", "Your code: 1234")
+
+email.send_to_phone("+996700000000", "Your code: 1234")
+email.send_to_email("test@gmail.com", "Your code: 1234")
 
